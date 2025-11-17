27ce8c6d5f239be233a082f56a4315cc59a61128
Miguel A Barbosa M <barbosamartinezmiguelangel1@gmail.com>
2025-11-16 22:34:54 -0500

    Primer Checkpoint, Tailwind V4 funciona

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <div className="relative mt-auto prose">
+        <h1 className='text-4xl text-red-500'>HOLA MUNDO</h1>
+        <p className="mt-3 leading-relaxed text-zinc-900">A la vívora vívora de la mar por aquí todos pueden pasar</p>
+      </div>
+    </>
+  )
+}
+
+export default App
