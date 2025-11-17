6215e44ee5b00d0e2fd9875a34a7b8110d6854c9
Miguel A Barbosa M <barbosamartinezmiguelangel1@gmail.com>
2025-11-17 15:06:24 -0500

    Igual que antes, pero ahora ya está el icono

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
     <div className="prose mx-auto mt-10">
       <h1>Home</h1>
       <p>Bienvenido. Esta es la página principal.</p>
-
+      
       <Link
         to="/login"
         className="px-4 py-2 bg-blue-600 text-white rounded"
